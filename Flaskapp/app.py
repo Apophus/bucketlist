afdc5685bb112a7cf5f3c2635fd1a9aad6676602
Apophus <bkilel12@gmail.com>
2016-06-08 14:43:09 +0300

    modified the main function to render the template

--- Flaskapp/app.py
+++ Flaskapp/app.py
@@ -4,11 +4,12 @@
 from flask import Flask
 app = Flask(__name__) #Flask will use this to determine root path of the app so it can find resource files relative to the app
 
+from flask import Flask, render_template #as the name suggests, we use it to render our template
 #define basic route and it's corresponding request handler
 @app.route('/')
 def main():
-    return "Welcome!"
+    return render_template('index.html')
 
 #checks if the executed file is the main program and runs the app
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run()
