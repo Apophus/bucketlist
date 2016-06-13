6e82973525dad93174e5b3ee86e8cb40166bfec8
Apophus <bkilel12@gmail.com>
2016-06-13 12:34:46 +0300

    rendered the signup template

--- Flaskapp/app.py
+++ Flaskapp/app.py
@@ -2,18 +2,22 @@
 
 #create the application instance
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__) #Flask will use this to determine root path of the app so it can find resource files relative to the app
 app.config.from_object('config')
 #db is the database
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
-from app import views,models
+#from app import views,models
 from flask import Flask, render_template #as the name suggests, we use it to render our template
 #define basic route and it's corresponding request handler
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
 
 #checks if the executed file is the main program and runs the app
 if __name__ == "__main__":
